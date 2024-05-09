aa07231c17f216ae9dc363ef514abe2fe6786aad
JOSÉ-ANTONIO-HD <hdzdomjoseantonio@gmail.com>
2024-05-08 23:15:41 -0600

    repodar la api

---
+++ constantes.js
@@ -0,0 +1,18 @@
+const DB_USER="basededatos";
+const DB_PASSWORD="admin123";
+const DB_HOST="cluster0.m7phttt.mongodb.net";
+const DB_NAME="basededatos";
+
+//const IP_SERVER="187.154.139.32";
+const IP_SERVER="127.0.0.1";
+
+const JWT_SECRET_KEY= "environment";
+
+module.exports={
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    IP_SERVER,
+    JWT_SECRET_KEY
+}
